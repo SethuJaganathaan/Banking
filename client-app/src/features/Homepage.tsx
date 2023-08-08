7dc666c48a79efd437dd12546f92870a449dfd47
Sethu J <sethuj99@gmail.com>
2023-08-08 13:15:46 +0530

    Homepage buttons and flex

--- client-app/src/features/Homepage.tsx
+++ client-app/src/features/Homepage.tsx
@@ -1,17 +1,30 @@
-import { Container } from "semantic-ui-react";
-
 export default function Homepage() {
-    return(
-        <>
-            <div className="parent">
-                <div className="child-1">
-                    Hi
-                </div>
+  return (
+    <>
+      <div className="Bamboo-homepage">
+        <div className="Bamboo-card-1">
+          <img width="100%" alt="logo" src="../assets/BaseBackground.png" />
+        </div>
 
-                <div className="child-2">
-                    hello
-                </div>
+        <div className="Bamboo-card-2">
+          <p className="Bamboo-card-welcome">Welcome</p>
+          <div className="Bamboo-card-content">
+            <div className="Bamboo-card-headers">
+              <h1 className="Bamboo-card-header">
+                Apply for the best products from Bamboo bank
+              </h1>
             </div>
-        </>
-    )
+
+            <div className="Bamboo-card-button-1">
+              <button className="Bamboo-card-button-1-1">Saving account</button>
+            </div>
+
+            <div className="Bamboo-card-button-2">
+              <button className="Bamboo-card-button-1-2">Current account</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
